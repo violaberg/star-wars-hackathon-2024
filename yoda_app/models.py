66656ff4173424df12ccbd02781246ee1ba9413a
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2024-05-23 19:13:33 +0200

    Add a base.html, static folder, template folder

--- yoda_app/models.py
+++ yoda_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    excerpt = models.TextField()
+    ingredients_content = models.TextField()
+    method_content = models.TextField()
+    is_published = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+    def number_of_likes(self):
+        return self.likes.count()
+    
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
--- yoda_app/views.py
+++ yoda_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from django.contrib import messages
+from django.views import generic, View
+from .models import Post, Comment
 
 # Create your views here.
-def index(request):
-    return HttpResponse("May the Force be with you!")
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(is_published=True)
+    template_name = 'yoda_app/index.html'
+    context_object_name = 'posts'
