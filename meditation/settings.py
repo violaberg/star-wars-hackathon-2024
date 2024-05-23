c9de3e2425c3a5c552ceb66039af75e01b8926a1
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-05-23 16:28:30 +0200

    refactor: Modify database config

--- meditation/settings.py
+++ meditation/settings.py
@@ -88,7 +88,10 @@ WSGI_APPLICATION = 'meditation.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
