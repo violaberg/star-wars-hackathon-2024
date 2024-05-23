a4626c6e6ba6c5e26d66e84ca504def6665db230
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2024-05-23 11:45:49 +0200

    Correting the path to index

--- meditation/settings.py
+++ meditation/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-x-e4t^i_%tuncl$kef%koj8(mgp##i2_%s+$dbfspl6v)m#$7f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 
+                 '.herokuapp.com']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'yoda_app',
 ]
 
