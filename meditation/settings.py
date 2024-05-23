5901962eefc285a957d993a3c7b2ce5136839acf
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2024-05-23 21:20:59 +0200

    feat: Configure authentification backends

--- meditation/settings.py
+++ meditation/settings.py
@@ -104,6 +104,14 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
