a5793da6d182f26c54326282023e37b0e83e7546
Viola Bergere <viola.bergere@gmail.com>
2024-05-25 15:08:46 +0000

    Style faq page and fix toggle

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-// Toggles frequently asked question answers
-document.addEventListener("DOMContentLoaded", function () {
-    var faqLink = document.querySelector(".faq-link");
-
-    faqLink.addEventListener("click", function (event) {
-        event.preventDefault();
-    });
-});
