cfdff463fd22a70941a85b303540f0f572dda09a
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-05-26 20:23:27 +0000

    Fix: remove Database configuration setup:settings

--- meditation/settings.py
+++ meditation/settings.py
@@ -109,6 +109,7 @@ DATABASES = {
     }
 }
 
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
